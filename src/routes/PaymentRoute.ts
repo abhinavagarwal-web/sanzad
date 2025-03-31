fd45546d222f512fa6d7487fea8bddad3e5ffffc
Abhinav Agarwal <62370646+abhinavagarwal-web@users.noreply.github.com>
2025-03-31 10:40:19 +0530

    Update PaymentRoute.ts

--- src/routes/PaymentRoute.ts
+++ src/routes/PaymentRoute.ts
@@ -1,10 +1,11 @@
 import express, {Request, Response, NextFunction, Router} from 'express'; 
-import { PaymentIniciate } from '../controllers/PaymentController';
+import { PaymentIniciate, PaymentWithReferenceNo } from '../controllers/PaymentController';
 import { PaymentStatusUpdate } from '../controllers/PaymentController';
 const router = express.Router(); 
 
 router.post('/payment-iniciate', PaymentIniciate); 
 router.post('/payment-status-update', PaymentStatusUpdate);
+router.post('/referencePayment', PaymentWithReferenceNo);
 
 
 export {router as PaymentRoute}; 
